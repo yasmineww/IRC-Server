19c242bf29d7692825530f450da17682c2007ff9
Yasmine <makhlouf.yasmine1@gmail.com>
2025-02-19 22:30:38 +0100

    Added Server_Socket_Creation to main()

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: ymakhlou <ymakhlou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:35:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2025/02/19 22:08:18 by ymakhlou         ###   ########.fr       */
+/*   Updated: 2025/02/19 22:27:38 by ymakhlou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void Server::JOINhandler(const std::vector<std::string> &data, int fd) {
     channel->addUser(user, fd);
     //need to add broadcast msg
 }
-// void Server::USERhandler(const std::vector<std::string> &data, int fd) { (void) fd; (void) data;}
+
 void Server::MODEhandler(const std::vector<std::string> &data, int fd) {(void) fd; (void) data;}
 
 std::vector<std::string> Server::getJoinedChannels(int fd)
@@ -219,87 +219,5 @@ int Server::Authenticate_User(int client_Id, int pos)
     };
     return (0);
 };
-
-std::string	Welcome_mssg(void)
-{
-	std::string welcome = GREEN;
-	welcome.append("\n");
-	welcome.append("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n");
-	welcome.append("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n");
-	welcome.append("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n");
-	welcome.append("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n");
-	welcome.append("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n");
-	welcome.append(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n");
-	welcome.append(YELLOW);
-	welcome.append("Login in to use LAYMONA OR you can send HELP to see the MANUAL.\n");
-	welcome.append(RESET);
-	return (welcome);
-};
-
-std::string	Server_Opening(void)
-{
-	std::string welcome = GREEN;
-	welcome.append("\n");
-	welcome.append("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n");
-	welcome.append("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n");
-	welcome.append("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n");
-	welcome.append("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n");
-	welcome.append("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n");
-	welcome.append(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n");
-	welcome.append(YELLOW);
-	welcome.append("Server is Loading ... \n\n");
-	welcome.append(RESET);
-	return (welcome);
-};
-
-void Server_Socket_Creation(std::string Port, std::string Pass_Code)
-{
-
-        cout << Server_Opening() << endl;
-
-        Server server_Cls ;
-        Client ForMulti_poll ;
-        server_Cls.bindSocket_str.sin_port = htons(atoi(Port.c_str()));
-        // Creation Of a socket, struct pollfd StrcPol
-        server_Cls.Server_PassCode = Pass_Code ;
-        server_Cls.socket_connection = socket(AF_INET, SOCK_STREAM, 0);
-        check_status(server_Cls.socket_connection, "Socket Connection Faild !");
-        server_Cls.bind_Arg = bind(server_Cls.socket_connection, (struct sockaddr *)&server_Cls.bindSocket_str, sizeof(server_Cls.bindSocket_str));
-        check_status(server_Cls.bind_Arg, "Bind Faild !");
-        server_Cls.Socket_listen = listen(server_Cls.socket_connection, 2);
-        check_status(server_Cls.Socket_listen, "Listen Faild !");
-
-        // the Client Struct For the Accept() function
-        struct sockaddr_in client_address;
-        socklen_t client_addr_len = sizeof(client_address);
-        std::pair<int, Client> TOADD ;
-
-        // Initialization Of the First Poll() Struct For the Server
-        server_Cls.poll_strc.fd = server_Cls.socket_connection ;
-        server_Cls.poll_strc.events = POLLIN ;
-        server_Cls.pollAr.push_back(server_Cls.poll_strc);
-        while (1)
-        {
-            server_Cls.poll_returnV = poll(server_Cls.pollAr.data(), server_Cls.pollAr.size(), -1);
-            if (server_Cls.poll_returnV > 0){
-                if (server_Cls.pollAr[0].revents & POLLIN) {
-                    server_Cls.acceptSocket_id = accept(server_Cls.socket_connection, (sockaddr *)&client_address, &client_addr_len);
-                    check_status(server_Cls.acceptSocket_id, "Accept Command Faild !");
-                    if (server_Cls.acceptSocket_id > 0) {
-                        SENDMESSAGE(Welcome_mssg(),server_Cls.acceptSocket_id) ;
-                        server_Cls.poll_strc.fd = server_Cls.acceptSocket_id ;
-                        server_Cls.poll_strc.events = POLLIN ;
-                        server_Cls.pollAr.push_back(server_Cls.poll_strc);
-                        TOADD.first = server_Cls.acceptSocket_id ;
-                        TOADD.second.fd = server_Cls.acceptSocket_id ; // Adding User Socker ID to the USER Struct
-                        server_Cls.Users.insert(TOADD);
-                    }
-                }
-            }
-            server_Cls.Check_client_Request();
-        }
-};
-
-
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,85 @@
 
 #include "Header/Macros.hpp"
 
+std::string	Welcome_mssg(void)
+{
+	std::string welcome = GREEN;
+	welcome.append("\n");
+	welcome.append("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n");
+	welcome.append("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n");
+	welcome.append("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n");
+	welcome.append("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n");
+	welcome.append("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n");
+	welcome.append(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n");
+	welcome.append(YELLOW);
+	welcome.append("Login in to use LAYMONA OR you can send HELP to see the MANUAL.\n");
+	welcome.append(RESET);
+	return (welcome);
+};
+
+std::string	Server_Opening(void)
+{
+	std::string welcome = GREEN;
+	welcome.append("\n");
+	welcome.append("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n");
+	welcome.append("██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n");
+	welcome.append("██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗\n");
+	welcome.append("██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝\n");
+	welcome.append("╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n");
+	welcome.append(" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n");
+	welcome.append(YELLOW);
+	welcome.append("Server is Loading ... \n\n");
+	welcome.append(RESET);
+	return (welcome);
+};
+
+void Server_Socket_Creation(std::string Port, std::string Pass_Code)
+{
+        cout << Server_Opening() << endl;
+
+        Server server_Cls ;
+        Client ForMulti_poll ;
+        server_Cls.bindSocket_str.sin_port = htons(atoi(Port.c_str()));
+        // Creation Of a socket, struct pollfd StrcPol
+        server_Cls.Server_PassCode = Pass_Code ;
+        server_Cls.socket_connection = socket(AF_INET, SOCK_STREAM, 0);
+        check_status(server_Cls.socket_connection, "Socket Connection Faild !");
+        server_Cls.bind_Arg = bind(server_Cls.socket_connection, (struct sockaddr *)&server_Cls.bindSocket_str, sizeof(server_Cls.bindSocket_str));
+        check_status(server_Cls.bind_Arg, "Bind Faild !");
+        server_Cls.Socket_listen = listen(server_Cls.socket_connection, 2);
+        check_status(server_Cls.Socket_listen, "Listen Faild !");
+
+        // the Client Struct For the Accept() function
+        struct sockaddr_in client_address;
+        socklen_t client_addr_len = sizeof(client_address);
+        std::pair<int, Client> TOADD ;
+
+        // Initialization Of the First Poll() Struct For the Server
+        server_Cls.poll_strc.fd = server_Cls.socket_connection ;
+        server_Cls.poll_strc.events = POLLIN ;
+        server_Cls.pollAr.push_back(server_Cls.poll_strc);
+        while (1)
+        {
+            server_Cls.poll_returnV = poll(server_Cls.pollAr.data(), server_Cls.pollAr.size(), -1);
+            if (server_Cls.poll_returnV > 0){
+                if (server_Cls.pollAr[0].revents & POLLIN) {
+                    server_Cls.acceptSocket_id = accept(server_Cls.socket_connection, (sockaddr *)&client_address, &client_addr_len);
+                    check_status(server_Cls.acceptSocket_id, "Accept Command Faild !");
+                    if (server_Cls.acceptSocket_id > 0) {
+                        SENDMESSAGE(Welcome_mssg(),server_Cls.acceptSocket_id) ;
+                        server_Cls.poll_strc.fd = server_Cls.acceptSocket_id ;
+                        server_Cls.poll_strc.events = POLLIN ;
+                        server_Cls.pollAr.push_back(server_Cls.poll_strc);
+                        TOADD.first = server_Cls.acceptSocket_id ;
+                        TOADD.second.fd = server_Cls.acceptSocket_id ; // Adding User Socker ID to the USER Struct
+                        server_Cls.Users.insert(TOADD);
+                    }
+                }
+            }
+            server_Cls.Check_client_Request();
+        }
+}
+
 int main (int argc, char **argv)
 {
     try
@@ -16,3 +95,5 @@ int main (int argc, char **argv)
     };
     return (0);
 };
+
+
