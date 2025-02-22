022a30d2399d537212c36fbd65a48d63df1328e8
Yasmine <makhlouf.yasmine1@gmail.com>
2025-02-23 00:30:15 +0100

    Removed comments fro NOTICE

--- Commands/Notice.cpp
+++ Commands/Notice.cpp
@@ -2,32 +2,27 @@
 #include "../Utils/Macros.hpp"
 
 void Server::NOTICEhandler(const std::vector<std::string> &data, int fd)
-// void NOTICE_command(std::string command, int fd, Server *Server_CLS)
 {
-    // Retrieve the client sending the NOTICE
     Client sender = Users[fd];
-    // Ensure the command has enough arguments: NOTICE <target> :<message>
     if (data.size() < 3)
-        return; // Unlike PRIVMSG, NOTICE does not return an error message !!
+        return; 
 
-    std::string target = data[1];  // User or channel
-    std::string message = data[2]; // Message content
+    std::string target = data[1]; 
+    std::string message = data[2];
 
-    // Check if target is a channel
+
     if (target[0] == '#' || target[0] == '&')
     {
         Channel *channel = getChannel(target);
         if (!channel)
-            return; // Do not return an error message
-
-        // Send the notice to all users in the channel except the sender
+            return;
         channel->broadcast(":" + sender.getNickName() + " NOTICE " + target + " :" + message + "\r\n");
     }
     else
-    { // Target is a user
+    {
         int receiver = getClientByName(target);
         if (receiver == -1)
-            return; // Do not return an error message
+            return;
         SENDMESSAGE(":" + sender.getNickName() + " NOTICE " + target + " :" + message + "\r\n", receiver);
     }
 }
